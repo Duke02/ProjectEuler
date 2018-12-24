7ad56a36a9ca95f175577a7fa37006c126f69f39
Trystan May <trystanmay@gmail.com>
2018-12-24 12:42:43 -0500

    Added comment to limit printing.

--- problem1.cpp
+++ problem1.cpp
@@ -4,7 +4,7 @@ int main() {
 	long sum = 0;
 	for (int i = 1; i < 1000; i++) {
 		if (i % 5 == 0 || i % 3 == 0) {
-			std::cout << i << std::endl;
+			// std::cout << i << std::endl;
 			sum += i;
 		}
 	}
